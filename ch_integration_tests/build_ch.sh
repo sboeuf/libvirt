359d52182975dd40f2aea136837d66cfa5e9271d
Sebastien Boeuf <sebastien.boeuf@intel.com>
2021-03-22 11:46:51 +0100

    ch_integration_tests: Simplify repository cloning
    Replace with git clone what was done with 4 lines of code.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- ch_integration_tests/build_ch.sh
+++ ch_integration_tests/build_ch.sh
@@ -3,11 +3,8 @@ set -x -e
 source $HOME/.cargo/env
 
 CH_VERSION="master"
-git init cloud-hypervisor
+git clone --depth 1 "https://github.com/cloud-hypervisor/cloud-hypervisor" -b "$CH_VERSION"
 pushd cloud-hypervisor
-git remote add origin https://github.com/cloud-hypervisor/cloud-hypervisor
-git fetch origin --depth 1 "$CH_VERSION"
-git checkout "$CH_VERSION"
 cargo build
 sudo cp target/debug/cloud-hypervisor /usr/local/bin
 sudo setcap cap_net_admin+ep /usr/local/bin/cloud-hypervisor
